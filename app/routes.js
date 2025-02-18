180cce5aa8d4bd0d7b75195fb5f75831cfc1dd85
jamesapallister <james.pallister2@nhs.net>
2025-02-18 14:39:16 +0000

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -5,14 +5,14 @@ const router = express.Router();
 
 // Add your routes here - above the module.exports line
 
-// Run this code when a form is submitted to 'magical-powers-answer'
-router.post('/magical-powers-answer', function (req, res) {
+// Run this code when a form is submitted to 'create-answer'
+router.post('/create-answer', function (req, res) {
 
 // Make a variable and give it the value from 'create'
-var magicPowers = req.session.data.create
+var create = req.session.data.create
 
 // Check whether the variable matches a condition
-if (magicPowers == "yes"){
+if (create == "yes"){
 // Send user to next page
 res.redirect('/question-page-mhs-one-page')
 } else {
